a47c92a067dd1dda8fb7098d5a32881414d77717
JOHN <65081425+jkimkim@users.noreply.github.com>
2021-12-26 13:39:11 +0300

    update

--- js/recap.js
+++ js/recap.js
@@ -259,3 +259,8 @@ function sortFruit() {
     }
 }
 sortFruit()
+
+let openBox = document.getElementById("box")
+openBox.addEventListener("click", function () {
+    console.log("I want to open the Box")
+})
