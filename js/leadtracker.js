782126d6decd1d94f871f9de81c8b61c7a470bad
JOHN <65081425+jkimkim@users.noreply.github.com>
2022-01-23 15:24:40 +0300

    Update leadtracker.js

--- js/leadtracker.js
+++ js/leadtracker.js
@@ -2,6 +2,9 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
-inputBtn.addEventListener("click", function() {
-    console.log("ouch")
+inputBtn.addEventListener("click", function () {
+    //push items to array
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+//log out items from input
