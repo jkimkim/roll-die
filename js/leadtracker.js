28d7b7fa0c65d46169aef8ed98ac29b38d571781
JOHN <65081425+jkimkim@users.noreply.github.com>
2022-01-23 15:43:02 +0300

    update

--- js/leadtracker.js
+++ js/leadtracker.js
@@ -1,13 +1,15 @@
-let myLeads = ["www.this.com" , "www.nomesd.com" , "www.coler.value"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     //push items to array
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    ulEl.textContent = + "- " + myLeads
 })
 //log out items from input
 for (i = 0; i < myLeads.length; i++){
-    console.log("- " + myLeads[i])
+    ulEl.textContent += "- " + myLeads
 }
